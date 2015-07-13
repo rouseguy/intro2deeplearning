acdfde9518cc8415098096b63420c9a570ece901
Raghotham <raghotham.s@gmail.com>
2015-07-13 23:46:59 +0530

    Updating script to untar new datasets added

--- download_data.sh
+++ download_data.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-mkdir data
-curl -Lo data/cifar.tar.gz https://www.dropbox.com/s/5n17fi55se58074/cifar.tar.gz?dl=1
-curl -Lo data/f_el_32_32_images.tar.gz https://www.dropbox.com/s/hplm0jft67a8wbk/f_el_32_32_images.tar.gz?dl=1
-
 cd data
+curl -Lo cifar.tar.gz https://www.dropbox.com/s/5n17fi55se58074/cifar.tar.gz?dl=1
+curl -Lo f_el_32_32_images.tar.gz https://www.dropbox.com/s/hplm0jft67a8wbk/f_el_32_32_images.tar.gz?dl=1
+
 tar -xvzf cifar.tar.gz
 tar -xvzf f_el_32_32_images.tar.gz
+tar -xvzf otto.tar.gz
+tar -xvzf blog_data.tar
